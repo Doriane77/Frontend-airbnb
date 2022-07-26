d30580988b5c1d20c55a25842f531dbd236551bc
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-26 23:02:42 +0200

    theme dark

---
+++ Styles/darkTheme.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const darkTheme = StyleSheet.create({
+  body: {
+    backgroundColor: "#222629",
+  },
+  color: {
+    // backgroundColor: "white",
+    color: "white",
+  },
+});
+export default darkTheme;
