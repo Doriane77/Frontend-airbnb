4e38d34ef8f59327abe05678616499a93c9bb962
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-26 23:03:14 +0200

    style homescreen

---
+++ Styles/home.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const home = StyleSheet.create({
+  search: {
+    // backgroundColor: "gray",
+    borderColor: "red",
+    borderWidth: 1,
+    padding: 1,
+    paddingLeft: 10,
+    margin: 3,
+    width: 200,
+    borderRadius: 10,
+  },
+});
+export default home;
