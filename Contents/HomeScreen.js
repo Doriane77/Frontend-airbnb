451efb81708fb93a53da134cc81e974164d3fcde
Doriane77 <dlolliadu77@hotmail.fr>
2022-07-30 22:17:02 +0200

    all room and their users

--- Contents/HomeScreen.js
+++ Contents/HomeScreen.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, TextInput, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  FlatList,
+  Image,
+} from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import styles from "../Styles/Styles";
@@ -9,29 +16,34 @@ import home from "../Styles/home";
 import handleChange from "../Function/handleChange";
 
 import { URL_APP_API } from "@env";
+import StatusLoading from "../Components/StatusLoading";
 const axios = require("axios");
 
 function HomeScren({ theme }) {
   // const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   const [formValue, setFormValue] = useState({
     search: "",
     page: 1,
     limit: 10,
   });
   const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
   const { search, page, limit } = formValue;
   console.log("search :", search, "page :", page, "limit :", limit);
-  console.log("data :", data);
+  // console.log("rooms :", data);
 
   async function allRooms() {
     try {
       const response = await axios.get(`${URL_APP_API}rooms`);
+      const resUsers = await axios.get(`${URL_APP_API}users`);
+      setUsers(resUsers.data);
       setData(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
   }
-
   useEffect(async () => {
     allRooms();
   }, []);
@@ -56,17 +68,38 @@ function HomeScren({ theme }) {
           value={search}
         />
       </View>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text>BOX DES ROOMS</Text>
-        {data.map((room, index) => {
-          console.log(room);
-          return (
-            <View>
-              <Text>{room.title}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
+      {isLoading ? (
+        <StatusLoading />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => {
+            return (
+              <View style={home.box}>
+                <Image
+                  style={[home.imgRoom]}
+                  source={{ uri: item.photo.url }}
+                />
+                <Text>{item.title}</Text>
+                {users.map((elem, index) => {
+                  console.log(item.user == elem.id);
+                  return (
+                    <View key={elem.id}>
+                      {item.user == elem.id ? (
+                        <Image
+                          style={[home.imgUser]}
+                          source={{ uri: elem.photo.url }}
+                        />
+                      ) : null}
+                    </View>
+                  );
+                })}
+              </View>
+            );
+          }}
+          keyExtractor={(item) => String(item.id)}
+        />
+      )}
     </KeyboardAwareScrollView>
   );
 }
