2c894448d6302ca5d014719d094106bc7eef6796
Doriane77 <dlolliadu77@hotmail.fr>
2022-08-01 20:42:30 +0200

    dark theme profilescreen

--- Contents/ProfileScreen.js
+++ Contents/ProfileScreen.js
@@ -1,8 +1,30 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Image, Text, TextInput, View } from "react-native";
+import handleChange from "../Function/handleChange";
 
-function ProfileScreen() {
-  return <Text>Home page</Text>;
+import styles from "../Styles/Styles";
+import darktheme from "../Styles/darkTheme";
+
+function ProfileScreen({ theme }) {
+  const [formValue, setFormValue] = useState({
+    email: "",
+  });
+  const { email } = formValue;
+  return (
+    <View style={[styles.body, theme === true ? darktheme.body : ""]}>
+      {/* <Image source={}/> */}
+      <TextInput
+        inputType="email"
+        placeholder="new email"
+        placeholderTextColor={theme === true ? "white" : "gray"}
+        name="email"
+        onChangeText={(text) =>
+          handleChange("email", text, formValue, setFormValue)
+        }
+        value={email}
+      />
+    </View>
+  );
 }
 
 export default ProfileScreen;
