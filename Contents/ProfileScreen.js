8c3270b31b93e79b9e5ddb2899b310c8528258b5
Doriane77 <dlolliadu77@hotmail.fr>
2022-08-05 22:25:06 +0200

    Box img

--- Contents/ProfileScreen.js
+++ Contents/ProfileScreen.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import { Image, Text, TextInput, View } from "react-native";
+import { Image, Text, TextInput, View, TouchableOpacity } from "react-native";
 import handleChange from "../Function/handleChange";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 import styles from "../Styles/Styles";
 import darktheme from "../Styles/darkTheme";
+import profile from "../Styles/profile";
 
 function ProfileScreen({ theme }) {
   const [formValue, setFormValue] = useState({
@@ -18,7 +21,25 @@ function ProfileScreen({ theme }) {
         theme === true ? darktheme.body : "",
       ]}
     >
-      {/* <Image source={}/> */}
+      <View style={[profile.align]}>
+        <Image style={[profile.boxImg]} />
+        <View style={[profile.newImg]}>
+          <TouchableOpacity>
+            <MaterialIcons
+              name="add-a-photo"
+              size={34}
+              color={theme === true ? "white" : "tomato"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <MaterialIcons
+              name="add-photo-alternate"
+              size={34}
+              color={theme === true ? "white" : "tomato"}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
       <TextInput
         inputType="email"
         placeholder="new email"
