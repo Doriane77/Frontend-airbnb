846e47d99485a3fea3ade6516be5547e4469470f
Doriane77 <dlolliadu77@hotmail.fr>
2022-08-07 22:30:12 +0200

    css profile screen

--- Styles/profile.js
+++ Styles/profile.js
@@ -8,7 +8,12 @@ const profile = StyleSheet.create({
     borderWidth: 1,
     borderColor: "red",
   },
+  margin: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
   align: {
+    // backgroundColor: "purple",
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
@@ -19,6 +24,42 @@ const profile = StyleSheet.create({
     height: 120,
     alignItems: "center",
     justifyContent: "space-around",
+  },
+  input: {
+    width: 280,
+
+    paddingLeft: 5,
+    height: 40,
+    fontSize: 16,
+    // backgroundColor: "blue",
+  },
+  borderInput: {
+    marginBottom: 30,
+    borderBottomWidth: 1,
+    borderBottomColor: "#EB5A62",
+  },
+  eye: {
+    width: 30,
+  },
+  seePass: {
+    // backgroundColor: "red",
+    width: 250,
+  },
+  button: {
+    // backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    width: 180,
+    borderWidth: 1,
+    borderRadius: 30,
+    margin: 10,
+  },
+  lastButton: {
+    marginBottom: 40,
+  },
+  textButton: {
+    fontSize: 18,
   },
 });
 export default profile;
